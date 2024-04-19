6964b9df05cb07c353fe02b3060c4b0b701c7b70
Bakhtawar Fatima <165771736+1bakhtawar@users.noreply.github.com>
2024-04-19 21:28:28 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+//This line is called a shang. It tells the OS to run it with nodejs
+import inquirer from "inquirer";
+import chalk from "chalk";
+//Declare a constant "answer" and assign it to the result of inquirer.prompt function
+console.log(chalk.bold.cyanBright("\n \t \t Start the word count "));
+console.log("=".repeat(60));
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: chalk.bold.blue("Ënter your sentence to count the word")
+    }
+]);
+const word = answer.sentence.trim().split(" ");
+console.log("=".repeat(60));
+console.log(chalk.bold(" Sentence word "));
+//print the array of words to the console
+console.log(word);
+console.log("=".repeat(60));
+const myName = "    Muhammad Ali    ";
+// print the word count of the sentence to the console
+console.log(chalk.bold.blue `Your sentence word count is ${word.length}`);
